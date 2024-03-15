cee6e165183e257eeb4b4100c835b4586a1f8697
3bdalah <3bdallah.dev@gmail.com>
2024-03-16 00:59:11 +0200

    git init xstore

---
+++ src/app/Shared/bannar/bannar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bannar',
+  standalone: true,
+  imports: [],
+  templateUrl: './bannar.component.html',
+  styleUrl: './bannar.component.css'
+})
+export class BannarComponent {
+
+}
